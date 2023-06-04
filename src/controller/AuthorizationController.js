a1e85f8c092f918b8471d1bd8b604038b63f40a3
RadioBoys <thainguyen84425@gmail.com>
2023-06-05 05:09:36 +0700

    Add readme.md, use .env to change some private

--- src/controller/AuthorizationController.js
+++ src/controller/AuthorizationController.js
@@ -1,8 +1,11 @@
 const generateRandomString = require('../public/js/generateRandomString ');
 const querystring = require('querystring');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const clientId = 'c7bed8c4114c4f278d635f279740eab6';
-const clientSecret = 'eed95642448f4546850a83afec055c87';
+const clientId = process.env.SPOTIFY_CLIENT_ID;
+const clientSecret = process.env.SPOTIFY_CLIENT_SECRET;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 
 class AuthorizationController {
 
@@ -12,22 +15,15 @@ class AuthorizationController {
     }
 
     login(req, res) {
-        const redirect_uri = 'http://localhost:3030/auth/callback';
         // const redirect_uri = encodeURI(redirectUri);
-        const client_id = 'c7bed8c4114c4f278d635f279740eab6';
-        // const authorizations = 'https://accounts.spotify.com/authorize';
-        // const uri = `${authorizations}?client_id=${clientId}&response_type=code&redirect_uri=${redirect_uri}&show_dialog=true&scope=user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private`
         const scope = 'user-read-private user-read-email user-modify-playback-state user-read-playback-position user-library-read streaming user-read-playback-state user-read-recently-played playlist-read-private';
-        // window.location.href = uri;
-        var state = generateRandomString(16);
-        // res.cookie(stateKey, state);
 
-        // your application requests authorization
-        // var scope = 'user-read-private user-read-email';
+        var state = generateRandomString(16);
+
         res.redirect('https://accounts.spotify.com/authorize?' +
             querystring.stringify({
                 response_type: 'code',
-                client_id: client_id,
+                client_id: clientId,
                 scope: scope,
                 redirect_uri: redirect_uri,
                 state: state
